7ab9e8ca291a4b4c2ec21a04e574850331afdc93
ramesh96420 <ramesh96420@gmail.com>
2018-11-03 22:12:09 +0530

    I am updating for sample purposs

--- src/main/java/SimpleHello.java
+++ src/main/java/SimpleHello.java
@@ -2,5 +2,7 @@ public class SimpleHello {
 
     public static void main(String[] args) {
         System.out.println("Hello..........");
+
+        System.out.println("ramesh............rame");
     }
 }
